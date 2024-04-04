14ebd2ea083f88d5c977de9268c52cbf57fc980a
Emil Olszewski <eemil.olszewski@gmail.com>
2024-04-04 14:10:01 +0200

    FundWatcherProgram.run -> FundWatcherProgram.setup

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from program import FundWatcherProgram
 if __name__ == '__main__':
 
     prog = FundWatcherProgram()
-    prog.run()
+    prog.setup()
 
     application = FundWatcherApp(prog.get_funds_listing(), prog.get_links_dict())
     application.run()
