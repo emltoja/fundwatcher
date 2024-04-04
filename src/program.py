ec89a5c4a9c94f7366f1ef14bd260bb5510110a8
Emil Olszewski <eemil.olszewski@gmail.com>
2024-04-04 14:28:43 +0200

    Changed way caching works
    Now instead of caching based on hardcoded input,
    program caches data if there is yet no cache or
    if it's older than one week.

--- src/program.py
+++ src/program.py
@@ -1,13 +1,15 @@
 import sys
 import requests
 import webbrowser
+import datetime
+import json
 from bs4 import BeautifulSoup
 
 URL = "https://www.santander.pl/tfi/fundusze-inwestycyjne"
 # Get the terminal size
 FUND_LINK_CLASS_NAME = "sbptfi_fund_information_table__table-details-link"
 DICT_INIT_ERR_MSG = "Error: self.link_dict wasn't initialized. Run self._get_links_dict_from_resp() or self._get_links_dict_from_cache() first."
-CACHE_FILE_PATH = "..\\cached_data.txt"
+CACHE_FILE_PATH = ".\\cached_data.json"
 
 MYFUNDS = {
     "Santander Dłużny Krótkoterminowy",
@@ -39,10 +41,9 @@ class FundWatcherProgram:
 
         try:
             with open(self.cached_data_path, "r", encoding='utf-8') as file:
-                self.text = file.read()
-            soup = BeautifulSoup(self.text, "html.parser")
-            links = soup.find_all("a", class_=FUND_LINK_CLASS_NAME)
-            self.link_dict = {link.text: link['href'] for link in links}
+                data = json.load(file)
+                self.text = data['text']
+                self.link_dict = data['links']
 
         except FileNotFoundError:
             print("Warning: Cache file not found. Fetching data from the website.")
@@ -55,14 +56,28 @@ class FundWatcherProgram:
                 sys.exit(1)
 
 
-    def _cache_data(self) -> None:
+    def _cache_data(self, file=None) -> None:
 
         if not self.text:
             print(DICT_INIT_ERR_MSG)
 
-
-        with open(self.cached_data_path, "w", encoding="utf-8") as file:
-            file.write(self.text)
+        if file is None:
+            with open(self.cached_data_path, "w", encoding="utf-8") as f:
+                json.dump(
+                    {
+                        'text': self.text,
+                        'links': self.link_dict,
+                        'timestamp': datetime.datetime.now().isoformat()
+                    }, f
+                )
+        else: 
+            json.dump(
+                {
+                    'text': self.text,
+                    'links': self.link_dict,
+                    'timestamp': datetime.datetime.now().isoformat()
+                }, file
+            )
 
 
     def _open_link(self, fund_name: str) -> None :
@@ -80,14 +95,28 @@ class FundWatcherProgram:
         webbrowser.open('https://www.santander.pl' + link)
 
 
-    def setup(self, use_cache=True) -> None:
+    def setup(self) -> None:
 
-        if use_cache:
-            self._set_links_dict_from_cache()
-        else:
+        try: 
+            with open(self.cached_data_path, "r", encoding='utf-8') as file:
+                data = json.load(file)
+                timestamp = data['timestamp']
+                if datetime.datetime.fromisoformat(timestamp) < datetime.datetime.now() - datetime.timedelta(days=7):
+                    resp = requests.get(self.url, timeout=5)
+                    if resp.status_code == 200:
+                        self._set_links_dict_from_resp(resp)
+                        self._cache_data(file)
+                    else:
+                        print(f"Error: Unable to connect to the website. {resp.status_code}")
+                        sys.exit(1)
+                else:
+                    self._set_links_dict_from_cache()
+            
+        except FileNotFoundError:
             resp = requests.get(self.url, timeout=5)
             if resp.status_code == 200:
                 self._set_links_dict_from_resp(resp)
+                self._cache_data()
             else:
                 print(f"Error: Unable to connect to the website. {resp.status_code}")
                 sys.exit(1)
